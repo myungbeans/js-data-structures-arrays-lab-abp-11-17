73b28a864de14d494fc817bf35346a0890c9acde
Michael Myung <michael.h.myung@gmail.com>
2017-11-11 05:49:44 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // Write your solution here!
 const drivers=['Milo','Otis','Garfield']
 
-function destructivelyAppendDriver(){
-  drivers.push=('Ralph')
+function destructivelyAppendDriver(name){
+  drivers.push=(name)
 }
 // alternative code to try: const destructivelyAppendDriver=[...drivers,'Ralph']
